c6bffa054e2a1580d29ccb0be5fead2fac714fed
Arash Amirebrahimi <arash_amirebrahimi@yahoo.com>
2022-09-17 22:26:24 +0430

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
     def index
+        flash.now[:notice] = "Logged in Successfully"
+        flash.now[:alert] = "invalid"
+        #or flash.alert
+        #flashes persist for one more action before going away meaning after the seond click they will go away
+        #to fix that flash.now can be used
     end
 end
